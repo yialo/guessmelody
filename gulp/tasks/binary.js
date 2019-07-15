2e1d492148aaa39573b7b6d74018efa317a16c8f
Aleksei Arro <aaa2@softingmsk.ru>
2019-07-15 12:58:08 +0300

    Gulp 'binary' task: lastRun method added to src handler

--- gulp/tasks/binary.js
+++ gulp/tasks/binary.js
@@ -3,6 +3,6 @@
 module.exports = () =>
   $.gulp.task('binary', () =>
     $.gulp
-      .src($.path.source.binary, {base: `.`})
+      .src($.path.source.binary, {base: `.`}, {since: $.gulp.lastRun('binary')})
       .pipe($.gulp.dest($.path.output.root))
   );
