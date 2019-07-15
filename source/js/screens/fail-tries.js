44d258167e4891079607679caac8ddf02029c995
Aleksei Arro <polsck@gmail.com>
2019-07-15 22:02:50 +0300

    Tempales moved to JS modules

---
+++ source/js/screens/fail-tries.js
@@ -0,0 +1,13 @@
+import {render} from '../lib/util';
+
+const template =
+`<section class="result">
+  <div class="result__logo">
+    <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83">
+  </div>
+  <h2 class="result__title">Какая жалость!</h2>
+  <p class="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
+  <button class="result__replay" type="button">Попробовать ещё раз</button>
+</section>`;
+
+export default render(template);
