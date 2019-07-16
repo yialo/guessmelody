e81d15ce3de5469db2fbad186340635f591c6711
Aleksei Arro <aaa2@softingmsk.ru>
2019-07-16 13:15:06 +0300

    game-genre.js module logics added

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,4 @@
 import welcomeScreen from './screens/welcome';
 import {changeScreen} from './lib/util';
 
-export default function init() {
-  changeScreen(welcomeScreen);
-}
-
-init();
+changeScreen(welcomeScreen);
--- source/js/screens/game-genre.js
+++ source/js/screens/game-genre.js
@@ -81,13 +81,27 @@ const template =
 
 const container = render(template);
 
-const submitButton = container.querySelector('.game__submit');
-// submitButton.setAttribute('disabled', 'disabled');
-submitButton.addEventListener('click', () => changeScreen(gameArtistScreen));
+const form = container.querySelector('.game__tracks');
+const checkboxes = [...form.querySelectorAll('.game__input')];
+const submitButton = form.querySelector('.game__submit');
 
-/* const checkboxes = container.querySelectorAll('.game__input');
-checkboxes.forEach((el) => el.addEventListener('change', () =>
-  if // TODO: добавить проверку состояний
-)); */
+submitButton.setClickabilityState = function (isClickable) {
+  if (isClickable) this.removeAttribute('disabled');
+  else this.setAttribute('disabled', 'disabled');
+};
+submitButton.setClickabilityState(false);
+
+const checkSelectedCheckboxPresence = () => checkboxes.some((el) => el.checked);
+
+const onCheckboxChange = () => {
+  if (checkSelectedCheckboxPresence()) submitButton.setClickabilityState(true);
+  else submitButton.setClickabilityState(false);
+};
+checkboxes.forEach((el) => el.addEventListener('change', onCheckboxChange));
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  changeScreen(gameArtistScreen);
+});
 
 export default container;
