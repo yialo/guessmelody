d77c7befeb9ed43cf9932de35d39468b1ce95b34
Aleksei Arro <polsck@gmail.com>
2019-07-21 19:02:41 +0300

    calculateScore method refactored in accordance with FailFast methodology

--- source/js/data/data.js
+++ source/js/data/data.js
@@ -1,16 +1,17 @@
 const FAILURE_CODE = -1;
 const ATTEMPTS = 3;
-const AnswerCost = {RIGHT: 1, MISTAKE: 2};
+const AnswerCost = {RIGHT: 1, MISTAKE: 2, QUICK: 1};
 
 export function calculateScore(answers, attemptsRemain) {
+  if (!answers.every((it) => it.isGuessed)) return FAILURE_CODE;
+
   const pointsSum = answers
-    .map((answer) => {
-      if (answer.isGuessed) return AnswerCost.RIGHT;
-      return 0;
+    .map((it) => {
+      let score = AnswerCost.RIGHT;
+      if (it.time < 30) score += AnswerCost.QUICK;
+      return score;
     })
     .reduce((sum, it) => sum + it);
-
-  if (pointsSum < 10) return FAILURE_CODE;
 
   const mistakesDone = ATTEMPTS - attemptsRemain;
 
--- source/js/data/data.test.js
+++ source/js/data/data.test.js
@@ -6,18 +6,62 @@ describe('Player score calculator', () => {
     this.isGuessed = isGuessed;
     this.time = time;
   };
-  const answers1 = new Array(10).fill(new Answer(true, 32));
-  const answersData = [true, true, true, true, true, true, false, true, true, false];
-  const answers2 = answersData.map((it) => new Answer(it, 32));
+  const getAnswers = (data) => data.map((it) => new Answer(...it));
+
+  const answersData1 = [
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+  ];
+  const answersData2 = [
+    [true, 29],
+    [true, 3],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 12],
+  ];
+  const answersData3 = [
+    [true, 29],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [true, 30],
+    [false, 30],
+    [true, 30],
+    [true, 30],
+    [false, 30],
+  ];
+
+  const answers1 = getAnswers(answersData1);
+  const answers2 = getAnswers(answersData2);
+  const answers3 = getAnswers(answersData3);
 
   it('Return -1 if not all questions answered', () => {
-    assert.strictEqual(calculateScore(answers2, 3), -1);
-    assert.strictEqual(calculateScore(answers2, 2), -1);
-    assert.strictEqual(calculateScore(answers2, 1), -1);
+    assert.strictEqual(calculateScore(answers3, 3), -1);
+    assert.strictEqual(calculateScore(answers3, 2), -1);
+    assert.strictEqual(calculateScore(answers3, 1), -1);
   });
-  it('Returns final score providing for attempts remain', () => {
+  it('Returns final score providing for attempts remain and all the answers are slow', () => {
     assert.strictEqual(calculateScore(answers1, 3), 10);
     assert.strictEqual(calculateScore(answers1, 2), 8);
     assert.strictEqual(calculateScore(answers1, 1), 6);
+  });
+  it('Returns final score providing for attempts remain and some answers are quick', () => {
+    assert.strictEqual(calculateScore(answers2, 3), 13);
+    assert.strictEqual(calculateScore(answers2, 2), 11);
+    assert.strictEqual(calculateScore(answers2, 1), 9);
   });
 });
