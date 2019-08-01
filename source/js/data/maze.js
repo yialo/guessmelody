e6df6cec378d3483c002d3e3a42052a18b084cf8
Aleksei Arro <polsck@gmail.com>
2019-08-02 00:17:19 +0300

    Two new subfunctions developed via BDD

--- source/js/data/maze.js
+++ source/js/data/maze.js
@@ -1,7 +1,54 @@
-export default function findWay(maze) {
-  const way = [];
+const Coords = function (x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+export const findStartCoords = (maze) => {
+  for (let i = 0; i < maze.length; i++) {
+    const mazeRow = maze[i];
+    for (let j = 0; j < mazeRow.length; j++) {
+      if (mazeRow[j] === 2) {
+        return new Coords(j + 1, i + 1);
+      }
+    }
+  }
+  return -1;
+};
+
+export const checkPoint = (maze, coords) => {
+  const Size = {
+    ROWS: maze.length,
+    COLUMNS: maze[0].length,
+  };
+
+  if (
+    coords.x > Size.COLUMNS
+    || coords.y > Size.ROWS
+    || coords.x < 0
+    || coords.y < 0
+  ) return -1;
+
+  const currentValue = maze[coords.y - 1][coords.x - 1];
+
+  if (currentValue === 0) return 0;
+  if (currentValue === 3) return 1;
+  if (currentValue === 1) return -1;
+
+  return -1;
+};
+
+export default (maze) => {
+  const Size = {
+    ROWS: maze.length,
+    COLUMNS: maze[0].length,
+  };
+
+  const start = findStartCoords(maze);
+
+  if (start === -1) return 'There is no enter point!';
+
+  const wayStack = [start];
 
 
-
-  return way;
-}
+  return wayStack;
+};
--- source/js/data/maze.test.js
+++ source/js/data/maze.test.js
@@ -1,14 +1,84 @@
 import {assert} from 'chai';
-import findWay from './maze';
+import findWayFromMaze, {findStartCoords, checkPoint} from './maze';
 
+describe('Test function for finding maze start point', () => {
+  it('Returns an object with enter-point coordinates', () => {
+    const maze1 = [
+      [2, 0, 3],
+    ];
+    const coordsExpected1 = {x: 1, y: 1};
+
+    const maze2 = [
+      [0, 0, 1],
+      [0, 0, 2],
+    ];
+    const coordsExpected2 = {x: 3, y: 2};
+
+    const maze3 = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 3, 0],
+    ];
+    const coordsExpected3 = -1;
+
+    assert.deepEqual(findStartCoords(maze1), coordsExpected1);
+    assert.deepEqual(findStartCoords(maze2), coordsExpected2);
+    assert.deepEqual(findStartCoords(maze3), coordsExpected3);
+  });
+});
+
+describe('Test function that checks coords for accessibility', () => {
+  const maze = [
+    [2, 0, 3],
+    [0, 1, 0],
+  ];
+
+  it('Returns -1 if choosed direction is beyond the maze', () => {
+    const coords1 = {x: 1, y: 3};
+    const expected1 = -1;
+    assert.strictEqual(checkPoint(maze, coords1), expected1);
+
+    const coords2 = {x: 4, y: 2};
+    const expected2 = -1;
+    assert.strictEqual(checkPoint(maze, coords2), expected2);
+
+    const coords3 = {x: -1, y: 0};
+    const expected3 = -1;
+    assert.strictEqual(checkPoint(maze, coords3), expected3);
+  });
+
+  it('Returns -1 if choosed point contains the wall', () => {
+    const coords1 = {x: 2, y: 2};
+    const expected1 = -1;
+    assert.strictEqual(checkPoint(maze, coords1), expected1);
+  });
+
+  it('Returns 0 if choosed direction is possible', () => {
+    const coords1 = {x: 2, y: 1};
+    const expected1 = 0;
+    assert.strictEqual(checkPoint(maze, coords1), expected1);
+
+    const coords2 = {x: 1, y: 2};
+    const expected2 = 0;
+    assert.strictEqual(checkPoint(maze, coords2), expected2);
+  });
+
+  it('Returns 1 if choosed direction leads to exit', () => {
+    const coords1 = {x: 3, y: 1};
+    const expected1 = 1;
+    assert.strictEqual(checkPoint(maze, coords1), expected1);
+  });
+});
+
+/*
 describe('Test function for traverse through maze', () => {
   it('Returns an array of coordinates', () => {
     const maze1 = [
       [2, 0, 3],
-      [1, 1, 1],
     ];
-    const expectedWay = [[1, 1], [2, 1], [3, 1]];
+    const wayExpected = [[1, 1], [2, 1], [3, 1]];
 
-    assert.deepEqual(findWay(maze1), expectedWay);
+    assert.deepEqual(findWayFromMaze(maze1), wayExpected);
   });
 });
+*/
