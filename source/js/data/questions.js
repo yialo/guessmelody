43b4b401dfc3088146f02d679d99daf599ad7bb7
Aleksei Arro <polsck@gmail.com>
2019-08-17 22:43:49 +0300

    questions data added

---
+++ source/js/data/questions.js
@@ -0,0 +1,10 @@
+const QUESTION_DATA = {
+  tracksAmount: 5,
+  artists: [
+    'Пелагея',
+    'Краснознаменная дивизия имени моей бабушки',
+    'Lorde',
+  ],
+};
+
+export default QUESTION_DATA;
