6237339579e68107cd013c301ed90af1d7ff75bb
Aleksei Arro <polsck@gmail.com>
2019-08-25 21:44:12 +0300

    AbstractView class created

---
+++ source/js/views/abstract-view.js
@@ -0,0 +1,23 @@
+export default class AbstractView {
+  get template() {
+    throw new Error(`${this} method must be redefined in AbstractView class' descendant`);
+  }
+
+  render() {
+    const $container = document.createElement('template');
+    $container.innerHTML = this.template;
+    this._$ = $container.content.firstChild;
+  }
+
+  bind() {
+    throw new Error(`${this} method must be redefined in AbstractView class' descendant`);
+  }
+
+  get $() {
+    if (!this._$) {
+      this.render();
+    }
+    this.bind();
+    return this._$;
+  }
+}
