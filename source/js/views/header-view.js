87c3ac2a1d590bb486bb4c42e3782c9eed4fecbd
Aleksei Arro <aaa2@softingmsk.ru>
2019-08-29 09:46:31 +0300

    Packages updated, unnecessary class removed

--- source/js/views/header-view.js
+++ source/js/views/header-view.js
@@ -1,12 +1,6 @@
 import AbstractView from './abstract-view';
-import HeaderTimerView from './header-timer-view';
 
 export default class HeaderView extends AbstractView {
-  constructor() {
-    super();
-    this.timer = new HeaderTimerView();
-  }
-
   get template() {
     return (
       `<header class="game__header">
@@ -16,11 +10,13 @@ export default class HeaderView extends AbstractView {
         </a>
 
         <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-          <circle class="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
+          <circle class="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center;"/>
         </svg>
 
         <div class="timer__value">
-          ${this.timer.template}
+          <span class="timer__mins">${this.minutes}</span>
+          <span class="timer__dots">:</span>
+          <span class="timer__secs">${this.seconds}</span>
         </div>
         <div class="game__mistakes"></div>
       </header>`
