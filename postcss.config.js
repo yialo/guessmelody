628d984bf5f2e3a7ded53071e3ceb99086273cbe
Aleksei Arro <polsck@gmail.com>
2019-09-28 22:37:25 +0300

    [Deploy] PostCSS config file reworked to better readability

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,24 @@
 'use strict';
 
-const advancedVars = require('postcss-advanced-variables');
-const colorFunc = require('postcss-color-function');
-const importer = require('postcss-import');
-const minify = require('cssnano');
 const normalize = require('postcss-normalize');
-const presetEnv = require('postcss-preset-env');
+
+const importConfig = normalize().postcssImport();
 
 module.exports = (ctx) => ({
-  plugins: [
-    importer(
-      normalize().postcssImport()
-    ),
-    advancedVars({
+  plugins: {
+    'postcss-import': importConfig,
+    'postcss-advanced-variables': {
       disable: ['@if', '@else', '@for', '@each'],
-    }),
-    colorFunc(),
-    presetEnv({
+    },
+    'postcss-color-function': {},
+    'postcss-preset-env': {
       stage: 3,
       features: {
         'custom-media-queries': true,
         'custom-selectors': true,
         'nesting-rules': true,
       },
-    }),
-    ctx.env === 'production' ? minify() : false,
-  ],
+    },
+    'cssnano': ctx.env === 'production' ? {} : false,
+  },
 });
