d9bf5c9b7a374325293e923c1b98929d58313e44
Aleksei Arro <polsck@gmail.com>
2019-12-24 21:05:48 +0300

    Drafts for AbstractView and errorUtil added

--- source/js/utils/errors.js
+++ source/js/utils/errors.js
@@ -1,3 +1,21 @@
+// export default {
+//   restrictConstructorCall: () => {
+//     throw new Error('Abstract class contructor call restricted');
+//   },
+
+//   requireMethodRedefinition: () => {
+//     throw new Error('Method must be defined in descendant class');
+//   },
+
+//   requireProp: (propName) => {
+//     throw new Error(`Required '${propName}' prop is not defined`);
+//   },
+
+//   badPropValue: (propName, propValue) => {
+//     throw new Error(`Unexpected value ${propValue} of prop ${propName}`);
+//   },
+// };
+
 export default {
   restrictAbstractCall() {
     throw new Error('Instantiation of abstract class is restricted');
--- source/js/views/_abstract-view.js
+++ source/js/views/_abstract-view.js
@@ -1,3 +1,122 @@
+// const { restrictConstructorCall, requireMethodRedefinition } = throwError;
+
+// export default class AbstractView {
+//   constructor(props = {}) {
+//     if (new.target === AbstractView) {
+//       restrictConstructorCall();
+//     }
+
+//     this._props = props;
+
+//     this._setHandlerNames();
+//     this._bindHandlers();
+//   }
+
+//   set $root($rootElement) {
+//     if (!this._$root) {
+//       this._$root = $rootElement;
+//     }
+//   }
+
+//   get template() {
+//     return requireMethodRedefinition();
+//   }
+
+//   get _isSingle() {
+//     return Boolean(this._$);
+//   }
+
+//   create($root) {
+//     this.createContainer();
+//     this.defineElements();
+//     this.activate();
+//     this.mount($root);
+//     this.performAfterMount();
+//   }
+
+//   destroy() {
+//     this.performBeforeUnmount();
+//     this.unmount();
+//     this.deactivate();
+//     this.undefineElements();
+//   }
+
+//   createContainer() {
+//     if (!this._$container) {
+//       this._$container = document.createElement('div');
+//     }
+//   }
+
+//   defineRoot($root) {
+//     if (!this._$root) {
+//       this._$root = $root;
+//     }
+//   }
+
+//   defineElements() {
+//     this._$container.innerHTML = this.template.trim();
+
+//     this._$children = [...this._$container.childNodes];
+//     const $firstChild = this._$children[0];
+
+//     if (this._$children.length === 1 && $firstChild.nodeType === 1) {
+//       this._$ = $firstChild;
+//     }
+//   }
+
+//   undefineElements() {
+//     if (this._$) {
+//       this._$ = null;
+//     }
+
+//     this._$children = null;
+//   }
+
+//   activate() {}
+
+//   deactivate() {}
+
+//   mount($root) {
+//     this.defineRoot($root);
+
+//     this._$children.forEach(($child) => {
+//       this._$root.appendChild($child);
+//     });
+//   }
+
+//   unmount() {
+//     this._$.remove();
+//   }
+
+//   performAfterMount() {}
+
+//   performBeforeUnmount() {}
+
+//   update() {}
+
+//   _setHandlerNames() {
+//     const nameArg = this._props.handlerNames;
+
+//     if (!nameArg) {
+//       this._handlerNames = null;
+//     } else if (typeof nameArg === 'string') {
+//       this._handlerNames = [nameArg];
+//     } else if (Array.isArray(nameArg)) {
+//       this._handlerNames = nameArg;
+//     } else {
+//       throw new Error(`Unexpected type of 'nameArg'`);
+//     }
+//   }
+
+//   _bindHandlers() {
+//     if (this._handlerNames) {
+//       this._handlerNames.forEach((name) => {
+//         this[`_${name}`] = this[`_${name}`].bind(this);
+//       });
+//     }
+//   }
+// }
+
 import errorUtil from '../utils/errors';
 
 export default class AbstractView {
