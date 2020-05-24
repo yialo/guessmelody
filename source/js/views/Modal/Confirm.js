c70aeb05c66e4a507871cea6a95dd74dd63b0b78
Aleksei Arro <polsck@gmail.com>
2020-05-24 20:15:29 +0300

    Move modal templates to JS, fix PDF

---
+++ source/js/views/Modal/Confirm.js
@@ -0,0 +1,11 @@
+const template = (`
+  <section class="modal">
+    <button class="modal__close" type="button"><span class="visually-hidden">Закрыть</span></button>
+    <h2 class="modal__title">Подтверждение</h2>
+    <p class="modal__text">Вы уверены что хотите начать игру заново?</p>
+    <div class="modal__buttons">
+      <button class="modal__button button">Ок</button>
+      <button class="modal__button button">Отмена</button>
+    </div>
+  </section>
+`);
---
+++ source/js/views/Modal/Error.js
@@ -0,0 +1,6 @@
+const template = (`
+  <section class="modal">
+    <h2 class="modal__title">Произошла ошибка!</h2>
+    <p class="modal__text">Статус: 404. Пожалуйста, перезагрузите страницу.</p>
+  </section>
+`);
